3884f29ac6bd0447c25dd90e6b779c59dbbe11ab
Giaki22 <giaki22@hotmail.com>
2022-07-07 09:06:19 +0200

    + Milestone 1 + Bonus

---
+++ resources/views/hey-you.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halo</title>
+</head>
+<body>
+    <h1>Salve!</h1>
+    <p>Ero anch'io un avventuriero come te, poi mi sono beccato una freccia nel ginocchio</p>
+    
+    <a href="/"><h1>Home</h1></a>
+</body>
+</html>
